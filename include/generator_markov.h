e38150f346fe7b21eb388909f3803b5d48ba7408
akalandiya <100804445+akalandiya@users.noreply.github.com>
2022-04-22 00:51:37 +0300

    Update

--- include/generator_markov.h
+++ include/generator_markov.h
@@ -5,18 +5,18 @@
 #include <vector>
 #include <map>
 
-using namespace std;
+//using namespace std;
 class genText {
- public:
-    string data;
-    string getText();
+public:
+    std::string data;
+    std::string getText();
 
-    vector<string> words;
-    typedef deque<string> prefix;  // prefix
-    map<prefix, vector<string> > statetab;  // prefix-suffix
+    std::vector<std::string> words;
+    typedef std::deque<std::string> prefix;  // prefix
+    std::map<prefix, std::vector<std::string> > statetab;  // prefix-suffix
     int NPREF;  // kol-vo slov s prefixe
     int MAXGEN;  // max kol-vo texta
-    genText(string filename, int i1, int i2);
-    genText(map<prefix, vector<string> > presuf,
-        vector<string> suf, int i1, int i2);
+    genText(std::string filename, int i1, int i2);
+    genText(std::map<prefix, std::vector<std::string> > presuf,
+        std::vector<std::string> suf, int i1, int i2);
 };
