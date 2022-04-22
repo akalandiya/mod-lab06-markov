9dbe6a8d040c4bdc0194eff534cd29bd856ab0f7
akalandiya <100804445+akalandiya@users.noreply.github.com>
2022-04-22 19:32:56 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -5,18 +5,15 @@
 #include <vector>
 #include <map>
 
-//using namespace std;
 class genText {
  public:
     std::string data;
     std::string getText();
 
     std::vector<std::string> words;
-    typedef std::deque<std::string> prefix;  // prefix
+    typedef std::deque<std::string> prefix;
     std::map<prefix, std::vector<std::string> > statetab;  // prefix-suffix
     int NPREF;  // kol-vo slov s prefixe
     int MAXGEN;  // max kol-vo texta
     genText(std::string filename, int i1, int i2);
-    genText(std::map<prefix, std::vector<std::string> > presuf,
-        std::vector<std::string> suf, int i1, int i2);
 };
